9518005f2ff67beccf6f5e46245a4e4a00efda16
Oli Hock <oliver.hock@gmail.com>
2024-01-29 17:24:48 +0100

    feat: Rename docker-compose.yml and update version to 3.4
    Renamed the docker-compose.yml file from address-book-compose/docker/docker-compose.yml to address-book-deployment/docker/docker-compose.yml. Also updated the version to 3.4.
    
    This commit deploys the complete application in a Docker Compose environment, including the PostgreSQL database, Spring Boot Backend application, and Angular Frontend application. All containers run in a network.
    
    The address-book-db service uses the postgres:16 image and exposes port 5432. The address-book-backend service uses the address-book-backend image, exposes port 7088, and mounts the .env file for environment variables.
    
    Note: Removed the unnecessary env_file entry for address-book-db service.

--- address-book-compose/k8s/postgres/postgres-port-forward.sh
+++
@@ -1,2 +0,0 @@
-kubectl get pods
-kubectl port-forward postgres-7f8868c6cd-v8d9d 6432:5432
--- address-book-compose/k8s/postgres/postgres-secret.sh
+++
@@ -1,4 +0,0 @@
-# Create a secret for the address book backend
-kubectl delete secret postgres-secret --ignore-not-found
-kubectl create secret generic postgres-secret --from-literal=SPRING_DATASOURCE_PASSWORD=postgres
-kubectl get secret postgres-secret -o yaml
